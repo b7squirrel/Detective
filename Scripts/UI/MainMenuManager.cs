10dfac17b79516df57f1797b74779b0d0f99a4a5
b7squirrel <b7squirrel@gmail.com>
2023-03-06 06:26:05 -0800

    탭 버튼 5개로 유지

--- Scripts/UI/MainMenuManager.cs
+++ Scripts/UI/MainMenuManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] Slider tabSlider;
-    [SerializeField] Slider tabSliderShadow;
     [SerializeField] GameObject[] tabPanels;
     [SerializeField] RectTransform[] BtnRect;
     [SerializeField] RectTransform[] BtnImageRect;
@@ -57,7 +56,6 @@ public class MainMenuManager : MonoBehaviour
     public void SetTabPos(int pressBtnID)
     {
         tabSlider.value = pos[pressBtnID];
-        tabSliderShadow.value = pos[pressBtnID];
         targetIndex = pressBtnID;
     }
 }
