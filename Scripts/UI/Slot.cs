e05382a77129d20eaa8aea3202429ea6302d3cb2
b7squirrel <b7squirrel@gmail.com>
2023-03-09 20:36:21 -0800

    상점에 물품 배치 작업 중

---
+++ Scripts/UI/Slot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Slot : MonoBehaviour
+{
+    ItemProperty item;
+    [SerializeField] Image image;
+
+    public void SetItem(ItemProperty item)
+    {
+        this.item = item;
+        if(item == null)
+        {
+            image.enabled = false;
+            gameObject.name = "Empty";
+        }
+        else
+        {
+            image.enabled = true;
+
+            gameObject.name = item.name;
+            image.sprite = item.sprite;
+        }
+    }
+}
---
+++ Scripts/UI/Store.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Store : MonoBehaviour
+{
+    [SerializeField] ItemBuffer itemBuffer;
+    [SerializeField] Transform slotRoot;
+    List<Slot> slots;
+
+    void Start() // 상점에 아이템을 배치하는 부분. 나중에 다시 셋업 해야함
+    {
+        slots = new List<Slot>();
+
+        int slotCnt = slotRoot.childCount;
+        for (int i = 0; i < slotCnt; i++)
+        {
+            Slot slot = slotRoot.GetChild(i).GetComponent<Slot>();
+
+            if (i < itemBuffer.items.Count)
+            {
+                slot.SetItem(itemBuffer.items[i]);
+            }
+
+            slots.Add(slot);
+        }
+    }
+    void Update()
+    {
+
+    }
+}
