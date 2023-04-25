eef7c5419882e834c5962dc574323241c4cff40b
b7squirrel <b7squirrel@gmail.com>
2023-04-25 01:19:01 -0700

    stage asset manager 추가
    - pool manager와 연결시켜야 하는데 잘 안됨
    - awake에서 초기화 시키는데 서로 엇갈리는 것 같음
    - pool manager의 awake를 start로 바꾸면 널오류가 계속 뜸

---
+++ Scripts/Managers/StageAssetManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageAssetManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject bossPrefab;
+    public GameObject[] effects;
+    public GameObject[] bossEffects;
+}
