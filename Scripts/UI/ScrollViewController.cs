86dd202b8c0178cfa16269ca043129a274c94a24
b7squirrel <b7squirrel@gmail.com>
2023-08-16 19:53:05 -0700

    all cards slot의 스크롤뷰 해결
    - mat cards slot에 스크롤을 넣어야 한다

--- Scripts/UI/ScrollViewController.cs
+++ Scripts/UI/ScrollViewController.cs
@@ -1,18 +1,27 @@
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScrollViewController : MonoBehaviour
 {
     ScrollRect scrollRect;
+    RectTransform gridLayout;
     void Awake()
     {
         scrollRect = GetComponent<ScrollRect>();
+        gridLayout = GetComponentInChildren<GridLayoutGroup>().GetComponent<RectTransform>();
     }
 
-    public void UpdateScrollView(int rowNum)
+    private void Update() {
+        UpdateScrollView();
+    }
+
+    public void UpdateScrollView()
     {
+        int rowNum = gridLayout.childCount / 4;
+        Debug.Log("row num = " + rowNum);
         if(rowNum == 0) return;
-        float height = rowNum * 300f;
+        float height = rowNum * 350f;
         scrollRect.content.sizeDelta = 
             new Vector2(scrollRect.content.sizeDelta.x, height);
     }
