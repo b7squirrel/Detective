1c60decf2b3eaba69c92d4d4ce58e2ac5110c053
b7squirrel <b7squirrel@gmail.com>
2023-10-06 14:18:05 -0700

    장비Display를 카드처럼 만들었음
    - 카드 프리펩으로 만들어야 나중에 카드 디자인을 바꿀 때 용이할 것 같음
    - 지금은 일단 넘어가자

--- Scripts/Equipments/Up/EquipDisplayUI.cs
+++ Scripts/Equipments/Up/EquipDisplayUI.cs
@@ -3,6 +3,12 @@ using UnityEngine.UI;
 
 public class EquipDisplayUI : MonoBehaviour
 {
+    [SerializeField] Transform cardBaseContainer; // 등급 5개 
+    [SerializeField] Transform starContainer;
+    [SerializeField] protected TMPro.TextMeshProUGUI Title;
+    [SerializeField] protected TMPro.TextMeshProUGUI Level;
+    [SerializeField] protected GameObject starPrefab;
+    GameObject[] stars;
     [SerializeField] Animator charImage;
     [SerializeField] Animator[] EquipmentImages;
     [SerializeField] Image HeadImage;
@@ -15,6 +21,24 @@ public class EquipDisplayUI : MonoBehaviour
 
     public void SetWeaponDisplay(CardData charCardData, OriAttribute currentAttr)
     {
+        // 별과 카드 색깔
+        cardBaseContainer.gameObject.SetActive(true);
+
+        int intGrade = new Convert().GradeToInt(charCardData.Grade);
+        SetNumStar(intGrade + 1);
+
+        for (int i = 0; i < 5; i++)
+        {
+            cardBaseContainer.GetChild(i).gameObject.SetActive(false);
+        }
+        cardBaseContainer.GetChild(intGrade).gameObject.SetActive(true);
+
+        // 카드 이름 텍스트
+        Title.text = charCardData.Name;
+        // 카드 레벨 텍스트
+        Level.text = "LV1";
+
+        // 오리 이미지
         charImage.gameObject.SetActive(true);
         WeaponData wd = cardDictionary.GetWeaponItemData(charCardData).weaponData;
         // charImage.sprite = wd.charImage;
@@ -50,8 +74,45 @@ public class EquipDisplayUI : MonoBehaviour
         charImage.Rebind();
     }
 
+    protected virtual void SetNumStar(int numStars)
+    {
+        if (stars == null)
+        {
+            // 5개 만들어서 비활성화
+            stars = new GameObject[5];
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = Instantiate(starPrefab, starContainer);
+                stars[i].SetActive(false);
+            }
+        }
+
+        // 등급만큼 별 활성화하고 별리스트에 넣기
+        for (int i = 0; i < numStars; i++)
+        {
+            stars[i].SetActive(true);
+        }
+    }
+
     public void OffDisplay()
     {
+        // 카드 Base Container 비활성화
+        cardBaseContainer.gameObject.SetActive(false);
+
+        // 별 비활성화
+        if (stars != null)
+        {
+            for (int i = 0; i < stars.Length; i++)
+            {
+                if (stars[i].activeSelf)
+                    stars[i].SetActive(false);
+            }
+        }
+
+        // 카드 레벨 텍스트
+        Level.text = "";
+        Title.text = "";
+
         atkLabel.SetActive(false);
         hpLabel.SetActive(false);
         // charImage.color = new Color(1, 1, 1, 0);
