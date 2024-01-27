8c6f001de9cc6cd7e984baca5fcd33b1d8893db0
b7squirrel <b7squirrel@gmail.com>
2024-01-27 15:54:44 -0800

    탭 색깔 파랑. 선택 탭은 노랑으로 했음

--- StartGame.cs
+++ StartGame.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,12 @@ public class StartGame : MonoBehaviour
 {
     public void StartGamePlay()
     {
+        StartCoroutine(LoadScenes());
+    }
+
+    IEnumerator LoadScenes()
+    {
+        yield return new WaitForSeconds(.15f); // 스타트 버튼의 Pressed 애니메이션의 재생시간 0.15초
         int currentStage = FindAnyObjectByType<PlayerDataManager>().GetCurrentStageNumber();
         string stageToPlay = "GamePlayStage" + currentStage.ToString();
         SceneManager.LoadScene("Essential", LoadSceneMode.Single);
