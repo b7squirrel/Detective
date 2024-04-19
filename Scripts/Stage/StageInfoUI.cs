2ffea1a6dade6c770add12f985e303ecb38c93cd
b7squirrel <b7squirrel@gmail.com>
2024-04-19 15:22:40 -0700

    탭 메뉴 아이콘 교체

--- Scripts/Stage/StageInfoUI.cs
+++ Scripts/Stage/StageInfoUI.cs
@@ -4,9 +4,12 @@ public class StageInfoUI : MonoBehaviour
 {
     [SerializeField] PlayerDataManager stageManager;
     [SerializeField] TMPro.TextMeshProUGUI Title;
+    [SerializeField] TMPro.TextMeshProUGUI stageNumber;
 
     internal void Init(Stages _currentStage)
     {
+        stageManager = FindObjectOfType<PlayerDataManager>();
         Title.text = _currentStage.Title;
+        stageNumber.text = "Stage " + stageManager.GetCurrentStageNumber().ToString();
     }
 }
