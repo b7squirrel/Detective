12b82c2e00ffb1d726bb7a019fe1c78c854f6592
b7squirrel <b7squirrel@gmail.com>
2024-10-20 14:10:56 -0700

    점선 벽

--- Scripts/Stage/BorderLines.cs
+++ Scripts/Stage/BorderLines.cs
@@ -5,13 +5,13 @@ public class BorderLines : MonoBehaviour
     [SerializeField] Transform [] rectangleTr; // 직사각형의 4개의 점
     [SerializeField] Transform[] walls; // 벽
     LineRenderer lineRenderer;
+    LineRenderer[] lineRenderers;
     Vector2[] rectanglePos = new Vector2[4];
 
     public void Init()
     {
         // LineRenderer 설정
-        if (lineRenderer == null) lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.positionCount = 5; // 직사각형은 4개의 점 + 시작점으로 돌아오는 마지막 점
+        if(lineRenderers == null ) lineRenderers = GetComponentsInChildren<LineRenderer>();
 
         rectanglePos[0] = new Vector2(walls[0].position.x, walls[3].position.y);
         rectanglePos[1] = new Vector2(walls[0].position.x, walls[2].position.y);
@@ -24,10 +24,13 @@ public class BorderLines : MonoBehaviour
     void DrawRectangle()
     {
         // 4개의 점을 지정하고 LineRenderer에 전달
-        lineRenderer.SetPosition(0, rectanglePos[0]); // 첫 번째 점
-        lineRenderer.SetPosition(1, rectanglePos[1]); // 두 번째 점
-        lineRenderer.SetPosition(2, rectanglePos[2]); // 세 번째 점
-        lineRenderer.SetPosition(3, rectanglePos[3]); // 네 번째 점
-        lineRenderer.SetPosition(4, rectanglePos[0]); // 마지막 점은 첫 번째 점으로 돌아옴
+        lineRenderers[0].SetPosition(0, rectanglePos[0]);
+        lineRenderers[0].SetPosition(1, rectanglePos[1]);
+        lineRenderers[1].SetPosition(0, rectanglePos[1]);
+        lineRenderers[1].SetPosition(1, rectanglePos[2]);
+        lineRenderers[2].SetPosition(0, rectanglePos[2]);
+        lineRenderers[2].SetPosition(1, rectanglePos[3]);
+        lineRenderers[3].SetPosition(0, rectanglePos[3]);
+        lineRenderers[3].SetPosition(1, rectanglePos[0]);
     }
 }
