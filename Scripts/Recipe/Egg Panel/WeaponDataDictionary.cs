6147ff5b4e274a2c5b2b07022b2612aa841229a1
DESKTOP-BP9IT6V\b7squ <b7squirrel@gmail.com>
2024-12-25 12:47:09 -0800

    등급별 무기데이터, 업그레이드 데이터 제작

--- Scripts/Recipe/Egg Panel/WeaponDataDictionary.cs
+++ Scripts/Recipe/Egg Panel/WeaponDataDictionary.cs
@@ -1,20 +1,43 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+[Serializable]
+public class AcquireDatas
+{
+    public string Name;
+    public UpgradeData[] acquireDatas;
+}
 
 public class WeaponDataDictionary : MonoBehaviour
 {
     [SerializeField] WeaponData[] weaponDatas;
-    [SerializeField] UpgradeData[] acquireDatas;
+    [SerializeField] List<UpgradeData> acquireDatas;
+    [SerializeField] List<AcquireDatas> AcquireDatas;
+    bool isInitDone;
 
     public UpgradeData GetAcquireDataFrom(string _weaponName, int _grade)
     {
-        for (int i = 0; i < acquireDatas.Length; i++)
+        if (isInitDone == false)
+        {
+            acquireDatas = new();
+            for (int i = 0; i < AcquireDatas.Count; i++)
+            {
+                acquireDatas.AddRange(AcquireDatas[i].acquireDatas);
+            }
+            isInitDone = true;
+        }
+
+        for (int i = 0; i < acquireDatas.Count; i++)
         {
             if (_weaponName == acquireDatas[i].weaponData.Name && _grade == acquireDatas[i].weaponData.grade)
             {
+                Debug.Log($"{_weaponName} 을 얻었습니다.");
                 return acquireDatas[i];
             }
         }
-        Debug.Log("무기 사전에 일치하는 오리가 없습니다.");
+        Debug.Log($"찾는 무기 => {_weaponName}, 무기 사전에 일치하는 오리가 없습니다.");
         return null;
     }
 }
