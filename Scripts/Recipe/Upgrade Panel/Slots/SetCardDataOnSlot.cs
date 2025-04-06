1ccb42cdce57d60ffa40f8c3d4ce47c3644c51b6
ori <b7squirrel@gmail.com>
2025-04-06 14:44:27 -0700

    Set Card Data On Slot의 SetEquipSpriteRow
    - WeaponItemData weaponItemData = cardDictionary.GetWeaponItemData(equipCardData); 부분이 중복되지 않게 했음

--- Scripts/Recipe/Upgrade Panel/Slots/SetCardDataOnSlot.cs
+++ Scripts/Recipe/Upgrade Panel/Slots/SetCardDataOnSlot.cs
@@ -32,7 +32,6 @@ public class SetCardDataOnSlot : MonoBehaviour
     {
         // card disp와 Equip Disp UI에서 IEquipSpriteAnim을 인터페이스로 사용
         IEquipSpriteAnim equipSpriteAnim = targetSlot.GetComponent<IEquipSpriteAnim>();
-        // CardDisp cardDisp = targetSlot.GetComponent<CardDisp>();
         equipSpriteAnim.InitSpriteRow(); // card sprite row의 이미지 참조들이 남지 않게 초기화
 
         EquipmentCard[] equipCards = cardList.GetEquipmentsCardData(charCard);
@@ -40,8 +39,6 @@ public class SetCardDataOnSlot : MonoBehaviour
         {
             if (equipCards[i] == null)
             {
-                // cardDisp.SetRunTimeAnimController(i, null);
-                // Debug.Log($"{charCard.Name}의 {i}번째 장비칸을 비활성화 합니다.");
                 equipSpriteAnim.SetEquipCardDisplay(i, null, false, Vector2.zero); // 이미지 오브젝트를 비활성화
                 continue;
             }
@@ -51,20 +48,13 @@ public class SetCardDataOnSlot : MonoBehaviour
             WeaponItemData weaponItemData = cardDictionary.GetWeaponItemData(equipCardData);
 
             if (weaponItemData.itemData == null)
-            {
-                // Debug.Log($"{charCard.Name}의 {i}번째 장비가 NULL입니다.");
                 continue;
-            }
-
-            // Debug.Log($"{weaponItemData.itemData.DisplayName}의 스프라이트를 넘겨줍니다.");
 
             // weapon data의 디폴트 아이템의 이미지를 넘겨주는 것이 아니라 장비 카드 데이터로 검색한 아이템의 이미지를 넘겨주어야 함.
-            Item item = cardDictionary.GetWeaponItemData(equipCardData).itemData;
-            SpriteRow equipmentSpriteRow = item.spriteRow;
+            Item item = weaponItemData.itemData;
             Vector2 offset = item.needToOffset ? item.posHead : Vector2.zero;
 
-            // Debug.Log($"{cardDictionary.GetWeaponItemData(equipCardData).itemData.DisplayName}을 디스플레이 할 것입니다.");
-            equipSpriteAnim.SetEquipCardDisplay(i, equipmentSpriteRow, item.needToOffset, offset);
+            equipSpriteAnim.SetEquipCardDisplay(i, item.spriteRow, item.needToOffset, offset);
         }
     }
 }
