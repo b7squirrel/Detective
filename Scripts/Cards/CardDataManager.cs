2fe85383fbd56790225e89b36c6f3a9296562a3d
ori <b7squirrel@gmail.com>
2025-04-13 17:51:26 -0700

    네임 스페이스 삭제
    - 자동으로 추가된 부분들 삭제

--- Scripts/Cards/CardDataManager.cs
+++ Scripts/Cards/CardDataManager.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using System;
 
 [System.Serializable]
 public class Serialization<T>
--- Scripts/Recipe/CardSlotManager.cs
+++ Scripts/Recipe/CardSlotManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.Serialization.Formatters;
 using UnityEngine;
-using VHierarchy.Libs;
 
 public class CardSlotManager : MonoBehaviour
 {
