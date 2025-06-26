1b446aec329c5e0842c7abfc4d7b852367455f5a
b7squirrel <b7squirrel@gmail.com>
2025-06-26 16:51:24 +0900

    slime drop 서서히 사라지도록

--- Scripts/Enemy/SlimeDrop.cs
+++ Scripts/Enemy/SlimeDrop.cs
@@ -1,9 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class SlimeDrop : MonoBehaviour
 {
+    [SerializeField] float life;
+    [SerializeField] GameObject mainBody; // 사라질 때 파괴할 오브젝트
+    [SerializeField] SpriteRenderer srBody, srBodyCore;
+    [SerializeField] float fadeDuration;
+    float lifeCounter;
     SlimeDropManager slimeDropManager;
     Animator anim;
+    #region OnTrigger
     void OnTriggerEnter2D(Collider2D collision)
     {
         CachingReferences();
@@ -25,4 +32,40 @@ public class SlimeDrop : MonoBehaviour
         if (slimeDropManager == null) slimeDropManager = FindObjectOfType<SlimeDropManager>();
         if (anim == null) anim = GetComponentInParent<Animator>();
     }
+    #endregion
+    
+    #region 수명
+    void Update()
+    {
+        Die();
+    }
+    void Die()
+    {
+        life -= Time.deltaTime;
+        if (life <= 0)
+        {
+            StartCoroutine(DieCo());
+        }
+    }
+    IEnumerator DieCo()
+    {
+        float elapsed = 0f;
+        Color originalColorBody = srBody.color;
+        Color originalColorBodyCore = srBodyCore.color;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsed / fadeDuration);
+            srBody.color = new Color(originalColorBody.r, originalColorBody.g, originalColorBody.b, alpha);
+            srBodyCore.color = new Color(originalColorBodyCore.r, originalColorBodyCore.g, originalColorBodyCore.b, alpha);
+            yield return null;
+        }
+
+        // 최종적으로 완전히 사라졌다면 비활성화하거나 제거할 수 있음
+        srBody.color = new Color(originalColorBody.r, originalColorBody.g, originalColorBody.b, 0f);
+            srBodyCore.color = new Color(originalColorBodyCore.r, originalColorBodyCore.g, originalColorBodyCore.b, 0f);
+        Destroy(mainBody);
+    }
+    #endregion
 }
