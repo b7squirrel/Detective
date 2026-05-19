1d096ea334d1e896f6d276ca28e2274d15615169
b7squirrel <b7squirrel@gmail.com>
2026-05-19 01:33:06 -0700

    라바스테이지 기믹 완료

--- Scripts/01_Managers/05_Stage/Fireball.cs
+++ Scripts/01_Managers/05_Stage/Fireball.cs
@@ -18,8 +18,17 @@ public class Fireball : MonoBehaviour
     [SerializeField] float fallDuration = 0.4f; // 위→착지 시간
 
     [Header("이펙트")]
-    [SerializeField] GameObject hitEffect;
+    [SerializeField] GameObject hitEffectPrefab;
     FireballIndicator indicator;
+
+    [Header("폭발 이펙트")]
+    [SerializeField] GameObject explosionEffectPrefab;
+
+    [Header("사운드")]
+    [SerializeField] AudioClip fallSound;
+    [SerializeField] AudioClip explosionSound;
+    [SerializeField][Range(0f, 1f)] float fallSoundVolume = 0.6f;
+    [SerializeField][Range(0f, 1f)] float explosionSoundVolume = 0.8f;
 
     void OnEnable()
     {
@@ -47,6 +56,10 @@ public class Fireball : MonoBehaviour
         Vector2 startPos = new Vector2(targetPos.x, targetPos.y + cameraHeight * 2f);
         transform.position = startPos;
 
+        // 낙하 사운드 재생
+        if (fallSound != null)
+            SoundManager.instance.PlaySoundWith(fallSound, fallSoundVolume, false, 0.3f);
+
         float elapsed = 0f;
 
         while (elapsed < fallDuration)
@@ -67,26 +80,50 @@ public class Fireball : MonoBehaviour
 
     void Explode()
     {
+        // indicator 크기를 폭발 범위로 사용
+        float range = explosionRange; // 기본값
+        if (indicator != null)
+            range = indicator.transform.localScale.x * 0.5f; // indicator 반지름
+
         if (indicator != null)
         {
             indicator.Hide();
             indicator = null;
         }
 
+        // 폭발 사운드
+        if (explosionSound != null)
+            SoundManager.instance.PlaySoundWith(explosionSound, explosionSoundVolume, false, 0.1f);
+
+        // 카메라 쉐이크
+        CameraShake.instance.Shake();
+
+        // 폭발 이펙트
+        if (explosionEffectPrefab != null)
+        {
+            GameObject effect = GameManager.instance.poolManager.GetMisc(explosionEffectPrefab);
+            if (effect != null)
+            {
+                effect.transform.position = transform.position;
+                StartCoroutine(DeactivateEffectCo(effect, 2f));
+            }
+        }
+
         // 적 데미지 + 넉백
         Collider2D[] enemyHits = Physics2D.OverlapCircleAll(
-            transform.position, explosionRange, LayerMask.GetMask("Enemy"));
+            transform.position, range, LayerMask.GetMask("Enemy"));
 
         foreach (var hit in enemyHits)
         {
             EnemyBase enemy = hit.GetComponent<EnemyBase>();
             if (enemy != null)
                 enemy.TakeDamage(enemyDamage, knockBackForce, knockBackDuration,
-                    transform.position, hitEffect);
+                    transform.position, hitEffectPrefab);
         }
 
+        // 플레이어 데미지 + 넉백
         Collider2D[] playerHits = Physics2D.OverlapCircleAll(
-            transform.position, explosionRange, LayerMask.GetMask("Player"));
+            transform.position, range, LayerMask.GetMask("Player"));
 
         foreach (var hit in playerHits)
         {
@@ -104,5 +141,11 @@ public class Fireball : MonoBehaviour
         }
 
         gameObject.SetActive(false);
+    }
+
+    IEnumerator DeactivateEffectCo(GameObject effect, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        effect.SetActive(false);
     }
 }
