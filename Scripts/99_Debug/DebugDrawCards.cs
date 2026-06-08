73148af75e37687ff137e4ecdcf5bcd336b19756
b7squirrel <b7squirrel@gmail.com>
2026-06-07 23:39:11 -0700

    클래식 요요 수염 -> 클래식 요요 안경으로 아이템 이름 바꿈

--- Scripts/99_Debug/DebugDrawCards.cs
+++ Scripts/99_Debug/DebugDrawCards.cs
@@ -232,7 +232,7 @@ public class DebugDrawCards : MonoBehaviour
     public void DrawItemCard()
     {
         gachaSystem.DrawSpecificCard("Item", itemIndex, itemGrade, itemNum, itemSkillIndex, itemEvoIndex);
-        Debug.LogError($"아이템 스킬 = {itemSkillIndex}");
+        Debug.Log($"[DebugDrawCard] 아이템 스킬 = {itemSkillIndex}");
     }
     #endregion
 }
