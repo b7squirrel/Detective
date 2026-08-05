3e3eb485c637a6bfd36a0301d7667f823f3f91ce
b7squirrel <b7squirrel@gmail.com>
2026-08-05 03:37:57 -0700

    총알 트레일

--- Scripts/12_Weapons/FireBallProjectile.cs
+++ Scripts/12_Weapons/FireBallProjectile.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 public class FireBallProjectile : ProjectileBase
 {
+    TrailRenderer trailRenderer;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        trailRenderer = GetComponent<TrailRenderer>();
+    }
+
+    private void OnDisable()
+    {
+        if (trailRenderer != null)
+            trailRenderer.Clear();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("MainCamera") || other.gameObject.CompareTag("Wall"))
